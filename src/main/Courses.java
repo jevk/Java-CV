1c0ab00a6201de3dfeeff1db2f35c4edd32281d0
Planks Wooden <planks@tutanota.com>
2023-03-30 12:18:56 +0300

    Update Courses.java

--- src/main/Courses.java
+++ src/main/Courses.java
@@ -211,5 +211,11 @@ public class Courses extends Information {
         });
         btnRemove.setBounds(202, 281, 98, 34);
         panel_1.add(btnRemove);
+        
+        JLabel lblNewLabel = new JLabel("Courses");
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+        lblNewLabel.setForeground(new Color(255, 255, 255));
+        lblNewLabel.setBounds(38, 11, 354, 49);
+        frame.getContentPane().add(lblNewLabel);
 	}
 }
