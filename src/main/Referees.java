1792245fb6d7d7e9ce9605020d8aaff03f43631e
Jevk <58623854+jevk@users.noreply.github.com>
2023-04-03 14:21:46 +0300

    Referees complete

--- src/main/Referees.java
+++ src/main/Referees.java
@@ -1,21 +1,37 @@
 package main;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class Referees {
+import structs.CV;
 
+public class Referees extends Information {
+
+	private String[] referees = new String[10];
 	private JFrame frame;
+	private CV cv;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args, CV curriculumVitae) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Referees window = new Referees();
+					Referees window = new Referees(curriculumVitae);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -27,7 +43,8 @@ public class Referees {
 	/**
 	 * Create the application.
 	 */
-	public Referees() {
+	public Referees(CV curriculumVitae) {
+		cv = curriculumVitae;
 		initialize();
 	}
 
@@ -35,9 +52,175 @@ public class Referees {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame = new JFrame();
+        frame.getContentPane().setBackground(new Color(39, 39, 39));
+        frame.getContentPane().setForeground(new Color(0, 0, 0));
+        frame.setBounds(100, 100, 800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(49, 49, 49));
+        panel.setBounds(408, 21, 354, 437);
+        frame.getContentPane().add(panel);
+        panel.setLayout(null);
+        
+        JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        tabbedPane.setBounds(10, 11, 334, 415);
+        panel.add(tabbedPane);
+        
+        JScrollPane detailsTab = new JScrollPane();
+        tabbedPane.addTab("Details", null, detailsTab, null);
+        
+        JTextArea detailsText = new JTextArea();
+        detailsText.setEditable(false);
+        detailsTab.setViewportView(detailsText);
+        
+        JScrollPane strengthsTab = new JScrollPane();
+        tabbedPane.addTab("Strengths", null, strengthsTab, null);
+        
+        JTextArea strengthsText = new JTextArea();
+        strengthsText.setEditable(false);
+        strengthsTab.setViewportView(strengthsText);
+        
+        JScrollPane degreeTab = new JScrollPane();
+        tabbedPane.addTab("Degrees", null, degreeTab, null);
+        
+        JTextArea degreeText = new JTextArea();
+        degreeText.setEditable(false);
+        degreeTab.setViewportView(degreeText);
+        
+        JScrollPane courseTab = new JScrollPane();
+        tabbedPane.addTab("Courses", null, courseTab, null);
+        
+        JTextArea courseText = new JTextArea();
+        courseText.setEditable(false);
+        courseTab.setViewportView(courseText);
+        
+        JScrollPane expTab = new JScrollPane();
+        tabbedPane.addTab("Experience", null, expTab, null);
+        
+        JTextArea expText = new JTextArea();
+        expTab.setViewportView(expText);
+        
+        JScrollPane itTab = new JScrollPane();
+        tabbedPane.addTab("IT Skills", null, itTab, null);
+        
+        JTextArea itText = new JTextArea();
+        itText.setEditable(false);
+        itTab.setViewportView(itText);
+        itText.setText("IT Skills:\n");
+        
+        JScrollPane langTab = new JScrollPane();
+        tabbedPane.addTab("Languages", null, langTab, null);
+        
+        JTextArea langsText = new JTextArea();
+        langsText.setEditable(false);
+        langTab.setViewportView(langsText);
+        
+        JScrollPane hobbyTab = new JScrollPane();
+        tabbedPane.addTab("Hobbies", null, hobbyTab, null);
+        
+        JTextArea hobbyText = new JTextArea();
+        hobbyTab.setViewportView(hobbyText);
+        
+        JScrollPane positionTab = new JScrollPane();
+        tabbedPane.addTab("Positions", null, positionTab, null);
+        
+        JTextArea positionText = new JTextArea();
+        positionTab.setViewportView(positionText);
+        
+        JScrollPane refereeTab = new JScrollPane();
+        tabbedPane.addTab("Referees", null, refereeTab, null);
+        
+        JTextArea refereeText = new JTextArea();
+        refereeTab.setViewportView(refereeText);
+        
+        getCV(cv, detailsText, strengthsText, degreeText, expText, itText, langsText, hobbyText, positionText, refereeText);
+        
+        JLabel lblNewLabel = new JLabel("Strengths");
+        lblNewLabel.setForeground(new Color(255, 255, 255));
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+        lblNewLabel.setBounds(28, 11, 354, 49);
+        frame.getContentPane().add(lblNewLabel);
+        
+        
+        JButton btnBack = new JButton("Back");
+        btnBack.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		cv.referees = referees;
+        		PersonalDetails.main(null, cv);
+        		frame.dispose();
+        	}
+        });
+        btnBack.setFont(new Font("Tahoma", Font.PLAIN, 13));
+        btnBack.setBounds(127, 504, 112, 23);
+        frame.getContentPane().add(btnBack);
+        
+        JPanel panel_1 = new JPanel();
+        panel_1.setBackground(new Color(49, 49, 49));
+        panel_1.setBounds(38, 71, 354, 387);
+        frame.getContentPane().add(panel_1);
+        panel_1.setLayout(null);
+        
+        JTextField refereeField = new JTextField();
+        refereeField.setBounds(64, 166, 225, 34);
+        panel_1.add(refereeField);
+        refereeField.setColumns(10);
+        
+        JButton btnAdd = new JButton("Add");
+        btnAdd.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		String strength = refereeField.getText();
+        		strengthsText.append(strength + "\n");
+        		for (int i = 0; i < referees.length; i++) {
+        			if (referees[i] == "" || referees[i] == null) {
+        				referees[i] = strength;
+        				break;
+        			}
+        		}
+        		cv.referees = referees;
+                getCV(cv, detailsText, strengthsText, degreeText, expText, itText, langsText, hobbyText, positionText, refereeText);
+        	}
+        });
+        btnAdd.setBounds(116, 236, 120, 34);
+        panel_1.add(btnAdd);
+        
+        JButton btnNext = new JButton("Next");
+        btnNext.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		cv.referees = referees;
+        		Degrees.main(null, cv);
+        		frame.dispose();
+        	}
+        });
+        btnNext.setFont(new Font("Tahoma", Font.PLAIN, 13));
+        btnNext.setBounds(545, 504, 112, 23);
+        frame.getContentPane().add(btnNext);
+        
+        
+        JLabel lblNewLabel_1 = new JLabel("Referee");
+        lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblNewLabel_1.setForeground(Color.WHITE);
+        lblNewLabel_1.setBounds(80, 115, 193, 40);
+        panel_1.add(lblNewLabel_1);
+        
+        JButton btnRemove = new JButton("Remove last entry");
+        btnRemove.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+           		for (int i = 0; i < 10; i++) {
+           			if (referees[i] == null || i == 10) {
+           				referees[i] = "";
+           				break;
+            		}
+            	}
+        		cv.referees = referees;
+                getCV(cv, detailsText, strengthsText, degreeText, expText, itText, langsText, hobbyText, positionText, refereeText);
+        	}
+        });
+        btnRemove.setBounds(116, 285, 121, 34);
+        panel_1.add(btnRemove);
 	}
 
 }
