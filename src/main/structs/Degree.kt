68d369a72a23d6f779173a50fa6c62969ebef2a1
jevk <jevk@users.noreply.github.com>
2023-05-23 00:48:27 +0300

    KOTLIN

---
+++ src/main/structs/Degree.kt
@@ -0,0 +1,16 @@
+package structs
+
+class Degree     // Constructors
+{
+    // Variable declarations
+	@kotlin.jvm.JvmField
+	var school = ""
+    @kotlin.jvm.JvmField
+	var education = ""
+    @kotlin.jvm.JvmField
+	var degree = ""
+    @kotlin.jvm.JvmField
+	var startYear = ""
+    @kotlin.jvm.JvmField
+	var endYear = ""
+}
