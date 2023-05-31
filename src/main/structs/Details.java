ca41a94118ab3b2b9196063323e93f42d029a525
Jevk <58623854+jevk@users.noreply.github.com>
2023-05-31 09:35:47 +0300

    Documented `Details.java` struct

--- src/main/structs/Details.java
+++ src/main/structs/Details.java
@@ -2,16 +2,24 @@ package structs;
 
 public class Details {
     // Variable declarations
+    // Store the user's name
     public String name;
+    // Store the user's address
     public String address;
+    // Store the user's zip code
     public String zip;
+    // Store the user's town
     public String town;
+    // Store the user's phone number
     public String phone;
+    // Store the user's email
     public String email;
+    // Store any other information the user wants to add (such as a website)
     public String other;
 
     // Constructors
     public Details() {
+        // Initialize variables
         this.name = "";
         this.address = "";
         this.zip = "";
